9e6ebbd805337ccd19b410a306f10b15c29405d4
helberthCO <h_caro.22@hotmail.com>
2020-01-27 19:04:10 -0500

    .gitignore added

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,30 +1,25 @@
-(function () {
-    
-    const API_URL = 'https://api.giphy.com/v1/gifs/trending';
-    const KEY = 'AGsk1t182PoJY4eVTiFGNXa2Blc0idb8';
-    const RANDOM = document.querySelector('#random')
+const API_URL = 'https://api.giphy.com/v1/gifs';
+const KEY = 'AGsk1t182PoJY4eVTiFGNXa2Blc0idb8';
+const RANDOM = document.querySelector('#random');
 
-    const apiCall = async (url, key, limit = 10) => {
-        const apiCall = await fetch(`${url}?api_key=${key}&limit=${limit}`);
-        const response = await apiCall.json();
-        return response;
-    }
+const apiCall = async (url, key, search, limit = 10) => {
+    const data = await fetch(`${url}/${search}?api_key=${key}&limit=${limit}`);
+    return await data.json();
+};
 
-    apiCall(API_URL, KEY, 45)
-        .then(allData => {
+apiCall(API_URL, KEY, 'trending', 45)
+    .then(allData => {
+        
+        const mainData = allData.data;
+        let eachGif = mainData.map(elem => elem.images);
+        let gifSize = eachGif.map(elem => elem['fixed_height']);
+        
+        gifSize.map(elem => {
             
-            const mainData = allData.data;
-            let eachGif = mainData.map(elem => elem.images);
-            let gifSize = eachGif.map(elem => elem['fixed_height_still']);
-            
-            gifSize.map(elem => {
-                
-                let imageTag = document.createElement('img');
+            let imageTag = document.createElement('img');
 
-                imageTag.setAttribute('src', elem.url);
-                RANDOM.appendChild(imageTag)    
-            })
+            imageTag.setAttribute('src', elem.url);
+            RANDOM.appendChild(imageTag)    
         })
-        .catch(error => console.log(error));
-
-})()
+    })
+    .catch(error => console.log(error));
